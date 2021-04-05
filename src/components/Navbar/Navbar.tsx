d2efa9af1c20213ad07b9299720b51a09291006f
Andrey Travin <andrey.gozon@mail.ru>
2021-04-05 22:14:37 +0300

    third commit

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,27 +1,27 @@
 import React from "react";
 import {NavLink} from "react-router-dom";
 
-import './Navbar.scss';
+import styles from './Navbar.module.scss';
 
 const Navbar = () => {
     return (
         <nav className={'navbar-wrapper'}>
-            <div className={'navbar-button'}>
+            <div className={styles.NavbarButton}>
                 <NavLink to={'./reports'}>Отчеты и графики</NavLink>
             </div>
-            <div className={'navbar-button'}>
+            <div className={styles.NavbarButton}>
                 <NavLink to={'./estimates'}>Входящие сметы</NavLink>
             </div>
-            <div className={'navbar-button'}>
+            <div className={styles.NavbarButton}>
                 <NavLink to={'./paymentCards'}>Карточки оплат</NavLink>
             </div>
-            <div className={'navbar-button'}>
+            <div className={styles.NavbarButton}>
                 <NavLink to={'./archiveCards'}>Архив карточек</NavLink>
             </div>
-            <div className={'navbar-button'}>
+            <div className={styles.NavbarButton}>
                 <NavLink to={'./counterparties'}>Контрагенты</NavLink>
             </div>
-            <div className={'navbar-button'}>
+            <div className={styles.NavbarButton}>
                 <NavLink to={'./changeHistory'}>История изменений</NavLink>
             </div>
         </nav>
